435ab20117a8c32dc891b9893d70f6ef4377d077
6 <skipper224483@gmail.com>
2022-04-30 09:08:42 +0300

    change vim colorscheme

---
+++ user/.config/nvim/lua/colors.lua
@@ -0,0 +1,16 @@
+local hi = vim.highlight.create
+vim.cmd "syntax enable"
+vim.o.background = "dark"
+vim.cmd "colorscheme peachpuff"
+
+
+hi("TabLineFill", {ctermfg="None", ctermbg="None", cterm="None"}, false)
+hi("TabLine", {ctermfg="Grey", ctermbg="None", cterm="None"}, false)
+hi("TabLineSel", {ctermfg="White", ctermbg="None", cterm="None"}, false)
+hi("StatusLine", {ctermfg="None", ctermbg="None", cterm="None"}, false)
+hi("StatusLineNC", {ctermfg="Yellow", ctermbg="None", cterm="None"}, false)
+hi("SignColumn", {ctermfg="White", ctermbg="None", cterm="None"}, false)
+hi("VertSplit", {ctermfg="Yellow", ctermbg="None", cterm="None"}, false)
+hi("Pmenu", {ctermfg="None", ctermbg="Black", cterm="None"}, false)
+hi("PmenuThumb", {ctermfg="Yellow", ctermbg="Black", cterm="None"}, false)
+hi("Todo", {ctermfg="Black", ctermbg="Red", cterm="None"}, false)
