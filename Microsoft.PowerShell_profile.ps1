1817630a7a40b0068f8eba3bcf4fd040a8a4e01f
e9 <skipper224483@gmail.com>
2022-08-05 19:36:59 +0300

    a

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,6 @@
 Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+function prompt {
+    $curDir = $(Get-Location)
+    write-host "$curDir" -ForegroundColor Green -NoNewline
+    ' '
+}
