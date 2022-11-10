f3d93125b5018be83f66614ec928a319a73fd15f
e9 <skipper224483@gmail.com>
2022-11-10 22:41:27 +0300

    a

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,18 @@
-Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+Set-PSReadLineOption -EditMode Emacs
 function prompt {
     $curDir = $(Get-Location)
     write-host "$curDir" -ForegroundColor Green -NoNewline
     ' '
+}
+
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
