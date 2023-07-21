3ef3e902c27daa735501fd3b26dd87c543348b21
e <skipper224483@gmail.com>
2023-07-21 08:40:27 +0300

    a

--- dwmconfig.h
+++ dwmconfig.h
@@ -10,8 +10,8 @@ static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#a8ff60";
+static const char col_gray4[]       = "#000000";
+static const char col_cyan[]        = "#fffaaa";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
